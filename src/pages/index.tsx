438f0553d35a7d6b105f5edefb718ac99e10855d
Steven Alvarenga <andersosuzukius@yahoo.com>
2023-04-28 02:14:45 -0300

    aaaa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ import web4 from "public/web4.png";
 import web5 from "public/web5.png";
 import web6 from "public/web6.png";
 import {useState} from "react";
-import Curriculum from 'next/curriculum';
+
 
 
 
